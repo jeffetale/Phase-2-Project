f6d8315aeb90cf13878d55f07f1b85e682fef454
jeff <jeff.etale@student.moringaschool.com>
2023-08-02 15:08:09 +0300

    added pet listing component

---
+++ src/components/PetListing.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PetListing = () => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("apiUrl")
+      .then((response) => {
+        setPets(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching pet data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Pet Listings</h2>
+      <div className="pet-list">
+        {pets.map((pet) => (
+          <div key={pet.id} className="pet-card">
+            <img src={pet.image} alt={pet.name} />
+            <h3>{pet.name}</h3>
+            <p>Breed: {pet.breed}</p>
+            <p>Age: {pet.age}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PetListing;
