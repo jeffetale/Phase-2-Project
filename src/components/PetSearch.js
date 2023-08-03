87b9142a08e7380f11c0e40c53308cc5af026a7b
jeff <jeff.etale@student.moringaschool.com>
2023-08-03 17:05:29 +0300

    animals can now be individually searched and displayed

--- src/components/PetSearch.js
+++ src/components/PetSearch.js
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const PetSearch = ({ onPetSelect }) => {
-  const [searchCriteria, setSearchCriteria] = useState({});
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const handleSearch = () => {
     axios
       .get("https://pets-api-yi52.onrender.com/pets", {
-        params: searchCriteria,
+        params: { q: searchValue },
       })
       .then((response) => {
-        setSearchResults(response.data);
+        onPetSelect(response.data[0]);
       })
       .catch((error) => {
         console.error("Error fetching search results:", error);
@@ -22,22 +21,16 @@ const PetSearch = ({ onPetSelect }) => {
     <div>
       <h2>Pet Search</h2>
       <div className="search-form">
+        <input
+          type="text"
+          placeholder="Search pets"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
         <button onClick={handleSearch}>Search</button>
-      </div>
-      <div className="search-results">
-        {searchResults.map((result) => (
-          <div key={result.id} className="result-card">
-            <img src={result.image} alt={result.name} />
-            <h3>{result.name}</h3>
-            <p>Breed: {result.breed}</p>
-            <p>Age: {result.age}</p>
-
-            <button onClick={() => onPetSelect(result)}>View Details</button>
-          </div>
-        ))}
       </div>
     </div>
   );
 };
+
 
 export default PetSearch;
