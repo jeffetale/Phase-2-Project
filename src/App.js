2d9dcbceb8730f2d5c072caed92d71b5d2f0c1c8
jeff <jeff.etale@student.moringaschool.com>
2023-08-04 04:30:53 +0300

    added a new route component with a path to the petID and used useParams to access pet ID from the API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
+import axios from "axios";
 import PetSearch from "./components/PetSearch";
 import PetDetails from "./components/PetDetails";
 import PetListing from "./components/PetListing";
@@ -70,10 +71,39 @@ const App = () => {
           <Route path="/add" element={<PetForm />} />
 
           <Route path="/favourites" element={<FavouritePets favourites={favourites} />} />
+
+          <Route
+            path="/details/:petId"
+            element={
+              <PetDetailsPage onBack={handleBack} onAddToFavourites={handleAddToFavourites} />
+            }
+          />
         </Routes>
 
       </div>
     </Router>
+  );
+};
+
+const PetDetailsPage = ({ onBack, onAddToFavourites }) => {
+  const [pet, setPet] = useState(null);
+  const { petId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`https://pets-api-yi52.onrender.com/pets/${petId}`)
+      .then((response) => {
+        setPet(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching pet:", error);
+      });
+  }, [petId]);
+
+  if (!pet) return null;
+
+  return (
+    <PetDetails pet={pet} onBack={onBack} onAddToFavourites={onAddToFavourites} />
   );
 };
 
