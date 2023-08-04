0df3db56f020fab5d405aa6cc06e48603f18470d
jeff <jeff.etale@student.moringaschool.com>
2023-08-04 03:49:39 +0300

    used react-router-dom for client side routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import PetSearch from "./components/PetSearch";
 import PetDetails from "./components/PetDetails";
 import PetListing from "./components/PetListing";
@@ -22,27 +23,58 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Pet Adoption App</h1>
-      <PetSearch onPetSelect={handlePetSelect} />
-      {selectedPets.length > 0 ? (
-        <>
-          {selectedPets.map((pet) => (
-            <PetDetails key={pet.id} pet={pet} onBack={handleBack} onAddToFavourites={handleAddToFavourites} />
-          ))}
-        </>
-      ) : (
-        <>
-          <PetListing onPetSelect={handlePetSelect} onAddToFavorites={handleAddToFavourites} />
-        </>
-      )}
-      <PetForm />
-      <FavouritePets favourites={favourites} />
+    <Router>
+      <div>
+        <h1>Pet Adoption App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Pet Search</Link>
+            </li>
+            <li>
+              <Link to="/add">Add a Pet</Link>
+            </li>
+            <li>
+              <Link to="/favourites">My Favourites</Link>
+            </li>
+          </ul>
+        </nav>
 
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PetListing onPetSelect={handlePetSelect} onAddToFavourites={handleAddToFavourites} />
+            }
+          />
+
+          <Route
+            path="/search"
+            element={
+              <>
+                <PetSearch onPetSelect={handlePetSelect} />
+                {selectedPets.length > 0 ? (
+                  <>
+                    {selectedPets.map((pet) => (
+                      <PetDetails key={pet.id} pet={pet} onBack={handleBack} onAddToFavourites={handleAddToFavourites} />
+                    ))}
+                  </>
+                ) : null}
+              </>
+            }
+          />
+
+          <Route path="/add" element={<PetForm />} />
+
+          <Route path="/favourites" element={<FavouritePets favourites={favourites} />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 };
 
 export default App;
-  
-
