71988d1044a8a0a8fd83d3650624ee0102b79dd9
jeff <jeff.etale@student.moringaschool.com>
2023-08-04 15:38:44 +0300

    added bootstrap styling

--- src/components/PetForm.js
+++ src/components/PetForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Form, Button, Container } from "react-bootstrap";
 
 const PetForm = () => {
   const [petData, setPetData] = useState({
@@ -38,86 +39,88 @@ const PetForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Add a Pet</h2>
-      <div>
-        <label>
-          Name:
-          <input
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <h2>Add a Pet</h2>
+        <Form.Group controlId="formName">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control
             type="text"
             name="name"
             value={petData.name}
             onChange={handleChange}
           />
-        </label>
-      </div>
-      <div>
-        <label>
-          Breed:
-          <input
+        </Form.Group>
+        <Form.Group controlId="formBreed">
+          <Form.Label>Breed:</Form.Label>
+          <Form.Control
             type="text"
             name="breed"
             value={petData.breed}
             onChange={handleChange}
           />
-        </label>
-      </div>
-      <div>
-        <label>
-          Age:
-          <input
+        </Form.Group>
+        <Form.Group controlId="formAge">
+          <Form.Label>Age:</Form.Label>
+          <Form.Control
             type="number"
             name="age"
             value={petData.age}
             onChange={handleChange}
           />
-        </label>
-      </div>
-      <div>
-        <label>
-          Gender:
-          <select name="gender" value={petData.gender} onChange={handleChange}>
+        </Form.Group>
+        <Form.Group controlId="formGender">
+          <Form.Label>Gender:</Form.Label>
+          <Form.Control
+            as="select"
+            name="gender"
+            value={petData.gender}
+            onChange={handleChange}
+          >
             <option value="">--Select Gender--</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Size:
-          <select name="size" value={petData.size} onChange={handleChange}>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formSize">
+          <Form.Label>Size:</Form.Label>
+          <Form.Control
+            as="select"
+            name="size"
+            value={petData.size}
+            onChange={handleChange}
+          >
             <option value="">--Select Size--</option>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Image URL:
-          <input
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formImage">
+          <Form.Label>Image URL:</Form.Label>
+          <Form.Control
             type="text"
             name="image"
             value={petData.image}
             onChange={handleChange}
           />
-        </label>
-      </div>
-      <div>
-        <label>
-          Description:
-          <textarea
+        </Form.Group>
+        <Form.Group controlId="formDescription">
+          <Form.Label>Description:</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
             name="description"
             value={petData.description}
             onChange={handleChange}
           />
-        </label>
-      </div>
+        </Form.Group>
 
-      <button type="submit">Add Pet</button>
-    </form>
+        <Button variant="primary" type="submit">
+          Add Pet
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
